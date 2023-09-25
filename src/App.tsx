f81b7597f2aad4cc226396dc5ac8def487f90b0e
Manoj Ranga <manojranga7583@gmail.com>
2023-09-25 14:02:22 +0530

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Header } from "./components/header-footer/Header";
+import { Footer } from "./components/header-footer/Footer";
+import { Home } from "./components/home/Home";
+
 function App() {
-  return <>Hi Manoj. Let's Begin</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;

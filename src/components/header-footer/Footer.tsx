4b7fbdc5ace917f050ef2fa708c9a222074bcf30
Manoj Ranga <manojranga7583@gmail.com>
2023-09-25 15:17:38 +0530

    feat: add footer

--- src/components/header-footer/Footer.tsx
+++ src/components/header-footer/Footer.tsx
@@ -1,5 +1,16 @@
+import { CityLogo } from "../utils/tools";
+
 function Footer() {
-  return <>Footer</>;
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo link={true} linkTo={"/"} width="70px" height="70px" />
+      </div>
+      <div className="footer_descl">
+        Manchester city 2023. All rights reserved
+      </div>
+    </footer>
+  );
 }
 
 export { Footer };
